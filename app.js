1a226133974479c68144fcfe857ca5e1cd37e11e
Egemen Nair <nairegemen@gmail.com>
2022-12-13 20:48:41 +0300

    Workspace created.

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+
+  const photo = {
+    id: 1,
+    name: "Photo Name",
+    description: "Photo Description"
+  }
+  res.send(photo);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`PCAT listening on port: ${port}`);
+});
