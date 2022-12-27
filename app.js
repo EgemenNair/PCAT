432faa92c1391830c901a7dbc6876e5a4c967937
Egemen Nair <nairegemen@gmail.com>
2022-12-27 21:19:17 +0300

    Add Page edited.

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 import express from 'express';
-import path from "path";
+import ejs from 'ejs';
+import path from 'path';
 
 const app = express();
 
+// Template Engine
+app.set('view engine', 'ejs');
+
 // Middlewares
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+// Routes
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/')
+});
 
 const port = 3000;
 app.listen(port, () => {
